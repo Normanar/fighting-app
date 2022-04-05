8071f4b8eab0cfc31aec20da41c9d51f39ae9647
Arman Alimov <527c@mail.ru>
2022-04-05 13:09:02 +0600

    add background and some refactoring

--- index.js
+++ index.js
@@ -8,65 +8,18 @@ const gravity = 0.7
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
-class Sprite {
-    constructor({position, velocity, color, offset}) {
-        this.position = position
-        this.velocity = velocity
-        this.height = 150
-        this.width = 50
-        this.color = color
-        this.lastKey
-        this.attackBox = {
-            position: {
-                x: this.position.x,
-                y: this.position.y,
-            },
-            offset,
-            width: 100,
-            height: 50,
-        }
-        this.isAttacking
-        this.health = 100
-    }
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0,
+    },
+    imageSrc : "./img/background.png"
+})
 
-    draw() {
-        c.fillStyle = this.color
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-
-        // attack box
-        if (this.isAttacking) {
-            c.fillRect(this.attackBox.position.x,
-                this.attackBox.position.y,
-                this.attackBox.width,
-                this.attackBox.height)
-        }
-    }
-
-    update() {
-        this.draw()
-
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
-        this.attackBox.position.y = this.position.y
-
-        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-            this.velocity.y = 0
-        } else {
-            this.velocity.y += gravity
-        }
-    }
-
-    attack() {
-        this.isAttacking = true
-        setTimeout(() => this.isAttacking = false, 100)
-    }
-}
-
-const player = new Sprite({
+const player = new Fighter({
     position: {
         x: 30,
-        y: 0,
+        y: 200,
     },
     velocity: {
         x: 0,
@@ -79,10 +32,10 @@ const player = new Sprite({
     }
 })
 
-const enemy = new Sprite({
+const enemy = new Fighter({
     position: {
         x: 944,
-        y: 100,
+        y: 200,
     },
     velocity: {
         x: 0,
@@ -110,49 +63,14 @@ const keys = {
     },
 }
 
-let jumpPlayer = true
-let jumpEnemy = true
 
-function playersCollision({attacking, defending}) {
-    return (
-        attacking.attackBox.position.x + attacking.attackBox.width >= defending.position.x
-        && attacking.attackBox.position.x <= defending.position.x + defending.width
-        && attacking.attackBox.position.y + attacking.attackBox.height >= defending.position.y
-        && attacking.attackBox.position.y <= defending.position.y + defending.height
-    )
-}
-
-function determineWinner({player, enemy}) {
-    document.querySelector('#fightingResult').style.display = "flex"
-    if (player.health === enemy.health) {
-        document.querySelector('#fightingResult').innerHTML = "Tie!"
-    } else if (player.health > enemy.health) {
-        document.querySelector('#fightingResult').innerHTML = "Player 1 win!"
-    } else {
-        document.querySelector('#fightingResult').innerHTML = "Player 2 win!"
-    }
-}
-
-let timer = 60;
-let timerId;
-
-function decreaseTimer() {
-    if (timer > 0) {
-        timerId = setTimeout(decreaseTimer, 1000)
-        timer -= 1
-        document.querySelector('#timer').innerHTML = timer + ''
-    }
-
-    if (timer === 0) {
-        determineWinner({player, enemy})
-    }
-}
 
 decreaseTimer()
 
 function animate() {
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
+    background.update()
     player.update()
     enemy.update()
     window.requestAnimationFrame(animate)
@@ -191,7 +109,7 @@ function animate() {
         document.querySelector('#playerHealth').style.width = player.health + '%'
     }
 
-    if(player.health <= 0 || enemy.health <= 0) {
+    if (player.health <= 0 || enemy.health <= 0) {
         determineWinner({player, enemy})
         clearTimeout(timerId)
     }
---
+++ js/utils.js
@@ -0,0 +1,37 @@
+let jumpPlayer = true
+let jumpEnemy = true
+
+function playersCollision({attacking, defending}) {
+    return (
+        attacking.attackBox.position.x + attacking.attackBox.width >= defending.position.x
+        && attacking.attackBox.position.x <= defending.position.x + defending.width
+        && attacking.attackBox.position.y + attacking.attackBox.height >= defending.position.y
+        && attacking.attackBox.position.y <= defending.position.y + defending.height
+    )
+}
+
+function determineWinner({player, enemy}) {
+    document.querySelector('#fightingResult').style.display = "flex"
+    if (player.health === enemy.health) {
+        document.querySelector('#fightingResult').innerHTML = "Tie!"
+    } else if (player.health > enemy.health) {
+        document.querySelector('#fightingResult').innerHTML = "Player 1 win!"
+    } else {
+        document.querySelector('#fightingResult').innerHTML = "Player 2 win!"
+    }
+}
+
+let timer = 60;
+let timerId;
+
+function decreaseTimer() {
+    if (timer > 0) {
+        timerId = setTimeout(decreaseTimer, 1000)
+        timer -= 1
+        document.querySelector('#timer').innerHTML = timer + ''
+    }
+
+    if (timer === 0) {
+        determineWinner({player, enemy})
+    }
+}
